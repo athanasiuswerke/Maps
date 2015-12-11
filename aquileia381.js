3fa2bd4cfffee21ce50a53879eba3ff2365f6846
Annette von Stockhausen <annette@vonstockhausen.eu>
2015-12-11 11:56:38 +0100

    Create aquileia381.js

---
+++ aquileia381.js
@@ -0,0 +1,229 @@
+/** Data: AW III 6, Dok. 93.8  – (c) Annette v. Stockhausen (CC-BY) **/
+/** Date: 10 Dec 2015 **/
+var bishops = {
+    "type": "FeatureCollection",
+    "features": [ 
+		{ 
+			"type": "Feature", 
+			"properties": {
+			"name": "Valerianus", 
+			"place": "Aquileia",
+			"uri": "http://pleiades.stoa.org/places/187290"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [13.370946, 45.769630999999997] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Ambrosius",
+			"place": "Mediolanum", 
+			"uri": "http://pleiades.stoa.org/places/383706"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [9.18806,45.463746] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Constantius",
+			"place": "Arausio", 
+			"uri": "http://pleiades.stoa.org/places/148054"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [4.807393,44.138048] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Iustus",
+			"place": "Lugdunum", 
+			"uri": "http://pleiades.stoa.org/places/167717"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [4.8194815,45.758866] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Abundantius",
+			"place": "Tridentum", 
+			"uri": "http://pleiades.stoa.org/places/383804"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [11.119135,46.0667685] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Eventius",
+			"place": "Ticinum", 
+			"uri": "http://pleiades.stoa.org/places/383798"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [9.156562,45.185899] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Limenius",
+			"place": "Vercellae", 
+			"uri": "http://pleiades.stoa.org/places/383815"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [8.426374,45.321094] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Eusebius",
+			"place": "Bononia", 
+			"uri": "http://pleiades.stoa.org/places/393421"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [11.3418224,44.4946274] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Sabinus",
+			"place": "Placentia", 
+			"uri": "http://pleiades.stoa.org/places/383741"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [9.696586,45.052752] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Exsuperantius",
+			"place": "Dertona", 
+			"uri": "http://pleiades.stoa.org/places/383633"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [8.865452,44.897171] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Bassianus",
+			"place": "Laus Pompeia", 
+			"uri": "http://pleiades.stoa.org/places/383691"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [9.418776,45.303456] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Anemius",
+			"place": "Sirmium", 
+			"uri": "http://pleiades.stoa.org/places/207447"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [19.610106,44.966447] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Constantius",
+			"place": "Siscia", 
+			"uri": "http://pleiades.stoa.org/places/197504"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [16.371388,45.483168] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Maximus",
+			"place": "Emona", 
+			"uri": "http://pleiades.stoa.org/places/197258"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [14.505963,46.051425] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Theodorus",
+			"place": "Octodurus", 
+			"uri": "http://pleiades.stoa.org/places/167859"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [7.07386025,46.0976681] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Domninus",
+			"place": "Gratianopolis", 
+			"uri": "http://pleiades.stoa.org/places/167734"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [5.732072,45.19261] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Diogenes",
+			"place": "Genava", 
+			"uri": "http://pleiades.stoa.org/places/177528"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [6.148505,46.201119] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Proculus",
+			"place": "Massilia", 
+			"uri": "http://pleiades.stoa.org/places/148127"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [5.382499,43.296854] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Heliodorus",
+			"place": "Altinum", 
+			"uri": "http://pleiades.stoa.org/places/393374"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [12.3994439,45.5462452] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Filastrius",
+			"place": "Brixia", 
+			"uri": "http://pleiades.stoa.org/places/383603"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [10.22625971,45.53992708] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Felix",
+			"place": "Iader", 
+			"uri": "http://pleiades.stoa.org/places/197312"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [15.223778,44.115501] 
+				}
+			},{ "type": "Feature", 
+			"properties": {
+			"name": "Amantius",
+			"place": "Luteva", 
+			"uri": "http://pleiades.stoa.org/places/148121"
+			}, 
+			"geometry": {
+				"type": "Point", 
+				"coordinates": [3.32373,43.7303755] 
+				}
+			}
+]
+};
